360439b7557ecb845e971b84a0c3ee0633796426
benjamin0809 <894306909@qq.com>
2019-04-16 22:18:03 +0800

    update favicon

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path'); 
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var favicon = require('serve-favicon')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -10,16 +11,18 @@ var hupuRouter = require('./routes/hupu');
 var app = express(); 
  
   
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.static(path.join(__dirname, 'public'))); 
+app.use(favicon(path.join(__dirname,  './favicon.ico')))
 // 自定义跨域中间件
 var allowCors = function(req, res, next) {
   res.header('Access-Control-Allow-Origin', req.headers.origin);
