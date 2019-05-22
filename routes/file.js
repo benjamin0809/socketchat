bf45fa8eed6be9329870255cb8adc1681e01f954
benjamin0809 <894306909@qq.com>
2019-05-22 22:42:36 +0800

    1

--- routes/file.js
+++ routes/file.js
@@ -4,168 +4,163 @@ var user = require('../modules/user/user')
 var utils = require('../utils/utils')
 var mutipart = require('connect-multiparty')
 const fs = require('fs')
-var multipartMiddleware = mutipart(); 
+var multipartMiddleware = mutipart();
 const path = require('path');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {  
-  res.sendFile(path.join(__dirname,'../modules/file/upload.html')) 
+router.get('/', function (req, res, next) {
+  res.sendFile(path.join(__dirname, '../modules/file/upload.html'))
 });
 
 /* GET users listing. */
-router.get('/download/demo', function(req, res, next) {  
-  res.sendFile(path.join(__dirname,'../modules/file/download.html')) 
-});
- 
- 
-router.post('/download/png', function(req, res, next) {  
-  res.sendFile(path.join(__dirname,'../public/images/album.png')) 
+router.get('/download/demo', function (req, res, next) {
+  res.sendFile(path.join(__dirname, '../modules/file/download.html'))
 });
 
-router.post('/download/pdf', function(req, res, next) {  
-  res.sendFile(path.join(__dirname,'../public/file/pdf.pdf')) 
+
+router.post('/download/png', function (req, res, next) {
+  res.sendFile(path.join(__dirname, '../public/images/album.png'))
 });
 
- 
-router.post('/download/word', function(req, res, next) {  
-  res.sendFile(path.join(__dirname,'../public/file/word.docx')) 
+router.post('/download/pdf', function (req, res, next) {
+  res.sendFile(path.join(__dirname, '../public/file/pdf.pdf'))
 });
 
-router.post('/download/excel', function(req, res, next) {  
-  res.sendFile(path.join(__dirname,'../public/file/language.xlsx')) 
+
+router.post('/download/word', function (req, res, next) {
+  res.sendFile(path.join(__dirname, '../public/file/word.docx'))
 });
 
-router.get('/download/getpdf', function(req, res, next) {  
-  res.sendFile(path.join(__dirname,'../public/file/pdf.pdf')) 
+router.post('/download/excel', function (req, res, next) {
+  res.sendFile(path.join(__dirname, '../public/file/language.xlsx'))
 });
 
-router.get('/download/getword', function(req, res, next) {  
-  res.sendFile(path.join(__dirname,'../public/file/word.docx')) 
-});
-router.get('/download/getexcel', function(req, res, next) {  
-  res.sendFile(path.join(__dirname,'../public/file/language.xlsx')) 
+router.get('/download/getpdf', function (req, res, next) {
+  res.sendFile(path.join(__dirname, '../public/file/pdf.pdf'))
 });
 
-router.post('/readStream/excel', function(req, res, next) {   
-  var readerStream = fs.createReadStream(path.join(__dirname,'../public/file/language.xlsx'));
+router.get('/download/getword', function (req, res, next) {
+  res.sendFile(path.join(__dirname, '../public/file/word.docx'))
+});
+router.get('/download/getexcel', function (req, res, next) {
+  res.sendFile(path.join(__dirname, '../public/file/language.xlsx'))
+});
+
+router.post('/readStream/excel', function (req, res, next) {
+  var readerStream = fs.createReadStream(path.join(__dirname, '../public/file/language.xlsx'));
 
   var data = ''
   readerStream.setEncoding('UTF8');
 
   // 处理流事件 --> data, end, and error
-  readerStream.on('data', function(chunk) {
+  readerStream.on('data', function (chunk) {
     data += chunk;
   });
 
-  readerStream.on('end',function(){
+  readerStream.on('end', function () {
     console.log(data);
     res.send(data)
- });
- 
- readerStream.on('error', function(err){
+  });
+
+  readerStream.on('error', function (err) {
     console.log(err.stack);
- });
+  });
 
 });
 
 
 /* GET home page. */
-router.post('/uploadFile',multipartMiddleware, function(req, res, next) {
-  console.log(req.body,req.files)  
-
+router.post('/uploadFile', multipartMiddleware, function (req, res, next) {
   const UPLOAD_PATH = '../public/upload'
-  let outpath = path.resolve(__dirname,UPLOAD_PATH)
+  let outpath = path.resolve(__dirname, UPLOAD_PATH)
+  if (!fs.existsSync(outpath)) {
+    fs.mkdirSync(outpath)
+  }
+  let result = 'Rendered to ' + JSON.stringify(req.files, null, 2) + '\n';
 
-  console.log(outpath)
- if(!fs.existsSync(outpath)){
-   fs.mkdirSync(outpath)
- } 
- let result = 'Rendered to ' + JSON.stringify(req.files, null, 2) + '\n';
-  
-  fs.copyFile(req.files.myfile.path, outpath + "\\" + req.files.myfile.originalFilename,fs.constants.COPYFILE_FICLONE,(err=>{
-    
-    if(err){
-     console.error(err) 
-    }else{  
-      
-     req.files.url = req.protocol + '://' + req.host + ':' + req.connection.localPort + '/upload/' + req.files.myfile.originalFilename
-     console.log(req.files.url)
-     console.log(req)
-     result += '<img src="'+ req.files.url +'">'
-    } 
-    res.writeHead(200, {'Content-Type' : 'text/html; charset=utf-8'});
-     res.end(result);
+  console.log('src:', req.files.myfile.path)
+  console.log('dest:', outpath + "\\" + req.files.myfile.originalFilename)
+  fs.copyFile(req.files.myfile.path, outpath + "\\" + req.files.myfile.originalFilename, fs.constants.COPYFILE_FICLONE, (err => {
+
+    if (err) {
+      console.error(err)
+    } else {
+
+      req.files.url = req.protocol + '://' + req.host + ':' + req.connection.localPort + '/upload/' + req.files.myfile.originalFilename
+
+      result += '<img src="' + req.files.url + '">'
+    }
+    res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+    res.end(result);
 
   }))
-  
-});
- 
-router.post('/saveAsHtml', function(req, res, next) {
-  console.log(req.body )  
 
-  if(req.body.token != '9527'){
+});
+
+router.post('/saveAsHtml', function (req, res, next) {
+  console.log(req.body)
+
+  if (req.body.token != '9527') {
     res.send({
       msg: 'token id invalid'
     })
     return;
-  } 
+  }
 
   let UPLOAD_PATH = '../public/upload'
   console.log(UPLOAD_PATH)
-  let outpath = path.resolve(__dirname,UPLOAD_PATH) 
+  let outpath = path.resolve(__dirname, UPLOAD_PATH)
   console.log(outpath)
- if(!fs.existsSync(outpath)){
-   fs.mkdirSync(outpath)
- }  
- 
-  
- let filename = req.body.filename || new Date().getTime()
- fs.writeFile(outpath + '/' + filename+ '.html' ,req.body.data,(err)=>{
-  if (err) {
+  if (!fs.existsSync(outpath)) {
+    fs.mkdirSync(outpath)
+  }
+
+
+  let filename = req.body.filename || new Date().getTime()
+  fs.writeFile(outpath + '/' + filename + '.html', req.body.data, (err) => {
+    if (err) {
+      res.send({
+        msg: 'o huo'
+      })
+      return;
+    }
     res.send({
-      msg: 'o huo'
+      result: filename
     })
-    return ;
-  } 
-  res.send({
-    result : filename
   })
- })
 
- router.post('/saveAsJS', function(req, res, next) {
-  console.log(req.body )  
+  router.post('/saveAsJS', function (req, res, next) {
+    console.log(req.body)
 
-  if(req.body.token != '9527'){
-    res.send({
-      msg: 'token id invalid'
-    })
-    return;
-  } 
+    if (req.body.token != '9527') {
+      res.send({
+        msg: 'token id invalid'
+      })
+      return;
+    }
 
-  let UPLOAD_PATH = '../public/js'
-  console.log(UPLOAD_PATH)
-  let outpath = path.resolve(__dirname,UPLOAD_PATH) 
-  console.log(outpath)
- if(!fs.existsSync(outpath)){
-   fs.mkdirSync(outpath)
- }  
+    let UPLOAD_PATH = '../public/js' 
+    let outpath = path.resolve(__dirname, UPLOAD_PATH) 
+    if (!fs.existsSync(outpath)) {
+      fs.mkdirSync(outpath)
+    }
  
-  
- let filename = req.body.filename || new Date().getTime()
- fs.writeFile(outpath + '/' + filename+ '.js' ,req.body.data,(err)=>{
-  if (err) {
-    res.send({
-      msg: 'o huo'
+
+    let filename = req.body.filename || new Date().getTime()
+    fs.writeFile(outpath + '/' + filename + '.js', req.body.data, (err) => {
+      if (err) {
+        res.send({
+          msg: 'o huo'
+        })
+        return;
+      }
+      res.send({
+        result: filename
+      })
     })
-    return ;
-  } 
-  res.send({
-    result : filename
+
   })
- })
-  
-})
-  
+
 });
 
 module.exports = router;
