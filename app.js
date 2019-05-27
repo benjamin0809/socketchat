900cf6a9217047b2754a482f8c243861178a0ecb
F2846595 <guo-xiang.zhao@foxconn.com>
2019-05-27 15:34:19 +0800

    update app

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var hupuRouter = require('./routes/hupu');
 var fileRouter = require('./routes/file');
+var mailRouter = require('./routes/mail');
 var app = express(); 
  
   
@@ -37,6 +38,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/hupu', hupuRouter);
 app.use('/file', fileRouter);
+app.use('/email', mailRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
