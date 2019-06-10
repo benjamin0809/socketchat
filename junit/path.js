b4c8302023d06df8b78e620998e895ef493d60d7
benjamin0809 <894306909@qq.com>
2019-06-10 23:18:05 +0800

    upload

---
+++ junit/path.js
@@ -0,0 +1,10 @@
+const path = require('path')
+
+
+console.log('public/upload',path.resolve('public/upload'))
+console.log('/public/upload',path.resolve('/public/upload'))
+console.log('./public/upload',path.resolve('./public/upload'))
+
+console.log('__dirname,public/upload',path.resolve(__dirname,'public/upload'))
+console.log('__dirname,/public/upload',path.resolve(__dirname,'/public/upload'))
+console.log('__dirname,./public/upload',path.resolve(__dirname,'./public/upload'))
