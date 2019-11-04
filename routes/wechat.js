8b4411434aab6216187093e4996ceb66ad2e22c5
Benjamin <894306909@qq.com>
2019-11-05 00:11:18 +0800

    update wechat

--- routes/wechat.js
+++ routes/wechat.js
@@ -2,14 +2,25 @@ var express = require('express');
 var router = express.Router(); 
 var utils = require('../utils/utils') 
 const http = require('../http/http.request')
+const { ResponseSuccess, ResponseError } = require('../models/response')
 
 const secret = "6d10f84578b70327e4f32fe468a88202"
 const appid = "wxc0d14756d7bce74f"
-router.post('/getOpenId', async (req, res, next) => {
+router.get('/getOpenId', async (req, res, next) => {
     const code = req.query.code
+
+    if(!code){
+        res.json(new ResponseError("code is empty").toJson())
+    }
     const url = `https://api.weixin.qq.com/sns/jscode2session?appid=${appid}&secret=${secret}&js_code=${code}&grant_type=authorization_code`
-    let res = http.get(url)
-    res.json(res)
+
+
+    try {
+        let result = http.get(url)
+        res.json(new ResponseSuccess(result).toJson())
+    } catch (e) {
+        res.json(new ResponseError(e).toJson())
+    } 
 }); 
 
 
