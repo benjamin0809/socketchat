5a111b66426842e90963cddf7adb2673305687c0
Benjamin <894306909@qq.com>
2019-11-05 00:53:02 +0800

    update wechat

--- routes/wechat.js
+++ routes/wechat.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var router = express.Router(); 
-var utils = require('../utils/utils') 
-const http = require('../http/http.request')
+const superagent = require('superagent');
+var http = require("http");
+var utils = require('../utils/utils')  
 const { ResponseSuccess, ResponseError } = require('../models/response')
 
 const secret = "6d10f84578b70327e4f32fe468a88202"
 const appid = "wxc0d14756d7bce74f"
+ 
+
 router.get('/getOpenId', async (req, res, next) => {
     const code = req.query.code
 
@@ -13,15 +16,21 @@ router.get('/getOpenId', async (req, res, next) => {
         res.json(new ResponseError("code is empty").toJson())
     }
     const url = `https://api.weixin.qq.com/sns/jscode2session?appid=${appid}&secret=${secret}&js_code=${code}&grant_type=authorization_code`
-
-
-    try {
-        let result = await http.get(url)
-        res.json(new ResponseSuccess(result).toJson())
-    } catch (e) {
-        res.json(new ResponseError(e).toJson())
-    } 
+     
+    superagent.get(url) 
+    .end((err, result) => {
+        if(err) {
+            res.json(new ResponseError(err).toJson())
+        } 
+        const testJSON = JSON.parse(result.text)
+        if(!testJSON.errcode){
+            res.json(new ResponseSuccess(testJSON).toJson()) 
+        }else{
+            res.json(new ResponseError(testJSON).toJson()) 
+        }
+        
+    })
+    
 }); 
-
 
 module.exports = router;
