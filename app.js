7d02b69c46dd06401f75e047b240a68fc4877ff2
F2846595 <guo-xiang.zhao@mail.foxconn.com>
2019-11-12 18:04:55 +0800

    add jwt-auth api

--- app.js
+++ app.js
@@ -35,18 +35,26 @@ app.use(express.static(path.join(__dirname, 'front/frontend')));
 app.use(express.static(path.join(__dirname, 'front'))); 
 app.use(favicon(path.join(__dirname,  './favicon.ico')))
 
+app.options("/*", function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  res.sendStatus(200);
+});
 
 // 自定义跨域中间件
 var allowCors = function(req, res, next) {
   // res.header('Access-Control-Allow-Origin', req.headers.origin); 
 
   res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
   res.header('Access-Control-Allow-Credentials','true');
   next();
 };  
-app.use(allowCors)
+
+app.all('*', allowCors);
+// app.use(allowCors)
 // 自定义跨域中间件
 
 
--- routes/login.js
+++ routes/login.js
@@ -5,8 +5,9 @@ var router = express.Router();
 var mutipart = require('connect-multiparty')
 const ArticleDao = require('../modules/article/article')
 const path = require('path');
-const dateUtils = require('../utils/date.utils')
+const dateUtils = require('../utils/date.utils') 
 const UserDao = require('../modules/user/user')
+const Token = require('../utils/token')
 var md5 = require('md5');
 
 router.get('/', function (req, res, next) {  
@@ -34,5 +35,68 @@ router.all('/login', async function (req, res, next) {
   } 
 });
  
+router.all('/extralogin', async function (req, res, next) { 
+    
+  let account = req.body.account || req.body.username
+  let password = req.body.password 
+   
+  if (account && password) {
+    const result = {
+      AccessToken: Token.generateUserToken(account),
+      RefreshToken: Token.generateAccessToken(account),
+      userId: account
+    }
+    res.json(result)
+  } else {
+    res.status(500).json({
+      msg: 'account infomation is incorret'
+    })
+  }
+ 
+});
 
+router.all('/getuser', async function (req, res, next) { 
+   
+   const accessToken = req.headers.authorization && req.headers.authorization.replace('Bearer ','')
+   
+   const UserID = Token.validToken(accessToken);
+
+   if(UserID) {
+    res.json({
+      account : UserID
+    });
+   } else{
+    res.status(401).end('unauthorization');
+   } 
+});
+router.all('/random', async function (req, res, next) { 
+   
+  const accessToken = req.headers.authorization && req.headers.authorization.replace('Bearer ','')
+  
+  const UserID = Token.validToken(accessToken);
+
+  if(UserID) {
+   res.json({
+     value : Math.random()
+   });
+  } else{
+   res.status(401).end('unauthorization');
+  } 
+});
+
+router.all('/refreshtoken', async function (req, res, next) { 
+  const refreshToken = req.body.refreshToken
+
+  const UserID = Token.validToken(refreshToken);
+
+   if(UserID) {
+    res.json({
+      jwt: Token.generateUserToken(UserID),
+      account : UserID
+    });
+   } else{
+    res.status(401).end('refreshtoken is invalid');
+   }  
+   
+});
 module.exports = router;
