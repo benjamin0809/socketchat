f7ca0e2813f2f7b45b312be378958993c4655dae
Benjamin Chiu <894306909@qq.com>
2020-03-22 17:59:27 +0800

    add

--- routes/avatar.js
+++ routes/avatar.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var router = express.Router();
+const FileUtils = require('../utils/file-utils')
+var mutipart = require('connect-multiparty')
+var multipartMiddleware = mutipart();
+const FileDao = require('../modules/file/file')
+const { ResponseSuccess, ResponseError } = require('../models/response')
 const fs = require('fs')
 const path = require('path')
 const defaultAvatar = '../public/upload/avatar/head.png'
@@ -13,4 +18,55 @@ router.get('/:id', function (req, res, next) {
   }
 })
  
+/* GET home page. */
+router.post('/save', multipartMiddleware, async function (req, res, next) {
+  const fileDao = new FileDao();
+  let fileEntity = fileDao.getInstance()
+
+  const UPLOAD_PATH = '../public/upload/avatar'
+  let outpath = path.resolve(__dirname, UPLOAD_PATH)
+  if (!fs.existsSync(outpath)) {
+    fs.mkdirSync(outpath)
+  }
+
+  const srcPath = req.files.myfile.path;
+  const destPath = outpath + "/" + req.body.id + ".png";
+
+  
+  fileEntity.filename = req.body.id + ".png";
+  fileEntity.filetype = req.files.myfile.type
+  fileEntity.fileSize = req.files.myfile.size
+  fileEntity.sourceUrl = req.files.url
+  fileEntity.path = destPath
+
+  var source = fs.createReadStream(srcPath); 
+  var dest = fs.createWriteStream(destPath);
+
+
+  source.pipe(dest);
+  source.on('end', function () {
+    fs.unlinkSync(srcPath); //delete tmp file
+    // res.writeHead(200, {
+    //   'Content-Type': 'application/json; charset=utf-8'
+    // });
+
+    let result = {}
+    let webUrl = FileUtils.getRequestUrl(req) + '/upload/avatar/' + req.body.id + ".png";
+    fileEntity.fullpath = webUrl
+    fileDao.insertFile(fileEntity).then(res => {
+      try {
+        result = res
+      } catch (e) { }
+    }).catch(e => {
+      try {
+        result = JSON.stringify(e)
+      } catch (e) { }
+    }) 
+    res.json(new ResponseSuccess(result).toJson());
+  });
+  source.on('error', function (err) {
+    res.json(new ResponseError().toJson());
+  });
+});
+
 module.exports = router;
