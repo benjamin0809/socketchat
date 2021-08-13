972db598427ffd53cd6da1a3d846366f81e9f256
benjamin0809 <894306909@qq.com>
2021-08-13 12:29:06 +0800

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const app =  express()
+const http = require('http')
 const path = require('path');  
 var loadDir = require('../../load_dir');
 var Module = loadDir('src/controllers'); 
@@ -19,6 +20,45 @@ router.get('/chatroom', function(req, res, next) {
 router.get('/menu', function(req, res, next) { 
   res.sendFile(path.join(__dirname, '../../public/menu.html'));
 }); 
+const getBonus = ()=>{ 
+  return new Promise((resolve, reject) => {
+    var opt = {  
+      headers:{
+        //这里放期望发送出去的请求头
+        "Referer": 'http://www.cwl.gov.cn/',
+        "Host": "www.cwl.gov.cn",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36"
+      }
+      }
+      //以下是接受数据的代码
+      var body = '';
+      var req = http.get('http://www.cwl.gov.cn/cwl_admin/kjxx/findDrawNotice?name=ssq&issueCount=1',opt, function(res) { 
+        res.on('data',function(d){
+        body += d;
+      }).on('end', function(){  
+        const json = JSON.parse(body)
+        if(json && json.state ==0) { 
+          resolve(json.result)
+        }
+      });
+    
+      }).on('error', function(e) {
+        console.log("Got error: " + e.message);
+        reject(e)
+      })
+      req.end();
+  })
+
+}
+router.get('/getBonus',async (req, res, next) => { 
+  try {
+    let result = await getBonus() 
+    res.json(result)
+  } catch(e){
+    res.error(e)
+  }
+ 
+})
 routers.push({path: '/',routerName: router}) 
 
 routers.forEach(item => { 
